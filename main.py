41fd6c8c3a0ad5342b2b7d15973ef09f652c9050
cyleja1234 <43104779+cyleja1234@users.noreply.github.com>
2021-03-10 21:09:40 -0700

    Update main.py
    made it cooler

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 #brain fuck+ by unknown81311#6616
-debug = False# for cool people
+debug = True
 pointer = 0
 pointerTwo = 0
-grid= [[0]*20 for _ in range(20)]# makes 20 x 20 grid
+grid= [[0]*1 for _ in range(5)]# 20 x 20 grid
 pc = 0
-code = "`" # set code here
-while pc < len(code):# loops through code
-    if code[pc] == "<":# for <
+code = "V`^[^][.V]" # set code here
+while pc < len(code):
+    if code[pc] == "<":
         pointer = pointer - 1
-    if code[pc] == ">":# for >
+    if code[pc] == ">":
         pointer = pointer + 1
-    if code[pc] == "V":# for V
+    if code[pc] == "V":
         pointerTwo = pointerTwo - 1
     if code[pc] == "+":# for +
-        if grid[pointerTwo][pointer] == 255:
-            grid[pointerTwo][pointer] = 0
-        else:
-            grid[pointerTwo][pointer] = grid[pointerTwo][pointer] + 1
+        grid[pointerTwo][pointer] = (grid[pointerTwo][pointer] + 1) % 256
     if code[pc] == "-":# for -
-        if grid[pointerTwo][pointer] == 0:
-            grid[pointerTwo][pointer] = 255
-        else:
-            grid[pointerTwo][pointer] = grid[pointerTwo][pointer] - 1
-    if code[pc] == "^":# for ^
+        grid[pointerTwo][pointer] = (grid[pointerTwo][pointer] - 1) % 256
+    if code[pc] == "^":
         pointerTwo = pointerTwo + 1
-    if code[pc] == "[":# for [
+    if code[pc] == "[":
         if grid[pointerTwo][pointer] == 0:
-            while code[pc] != "]":
+            pairs = 0
+            while pairs or code[pc] != "]":
+                if code[pc] == "[":
+                    pairs += 1
+                elif code[pc] == "]":
+                    pairs -= 1
                 pc = pc + 1
-    if code[pc] == "]":# for ]
+
+    if code[pc] == "]":
         if grid[pointerTwo][pointer] != 0:
-            while code[pc] != "[":
+            pairs = 0
+            while pairs or code[pc] != "[":
+                if code[pc] == "]":
+                    pairs += 1
+                elif code[pc] == "[":
+                    pairs -= 1
                 pc = pc - 1
-    if code[pc] == "`":# for `
+    if code[pc] == "`":
         var = map(ord, input())
         for char in var:
             grid[pointerTwo][pointer] = char
             pointerTwo = pointerTwo - 1
-    if code[pc] == ".":# for .
+    if code[pc] == ".":
         print(chr(grid[pointerTwo][pointer]))
-    if code[pc] == ",":# for ,
+    if code[pc] == ",":
         grid[pointerTwo][pointer] = input(ord(var))
-    pc = pc + 1# moves down the code string for the nect char.
+    pc = pc + 1
     if debug == True:
-        print (grid)
+        print (grid," ", code[pc-1])
