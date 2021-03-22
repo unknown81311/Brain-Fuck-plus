b1b044b824f281827db2055d242a67695ddfe98e
cyleja1234 <43104779+cyleja1234@users.noreply.github.com>
2021-03-21 22:19:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # brain fuck+ by unknown81311#6616 & NotGrey#2415
-import colorama
-colorama.init()
+
 def color(r=0, g=0, b=0):return f"\033[38;2;{r};{g};{b}m"
- 
+    
 def get_int(code, pc):
     i = ''
     code_s = len(code)
@@ -10,15 +9,14 @@ def get_int(code, pc):
         i += code[pc]
         pc += 1
     return int(i if i else '1'), len(i)
- 
- 
+    
+    
 def preprocess(code):
-    print(code)
     code_s = len(code)
     processed = ''
     pc = 0
     frames = []
- 
+    
     while pc < code_s:
         i, s = get_int(code, pc)
         if frames:
@@ -26,24 +24,24 @@ def preprocess(code):
         if s:
             pc += s
             continue
- 
+    
         if code[pc] in '#V^-+.,><':
             frames.append(code[pc])
         else:
             processed += code[pc]
- 
+    
         pc += 1
- 
+    
     if frames:
         processed += i * frames.pop()
- 
+    
     return processed
- 
- 
+    
+    
 def interpret(code, debug=False):
     pc, pointer, pointerTwo = 0, 0, 0
     grid = [[0] * WIDTH for _ in range(HEIGHT)]
- 
+    
     while pc < len(code):
         if code[pc] == "<":
             pointer = (pointer - 1) % WIDTH
@@ -57,7 +55,7 @@ def interpret(code, debug=False):
             grid[pointerTwo][pointer] = (grid[pointerTwo][pointer] - 1) % 256
         elif code[pc] == "+":
             grid[pointerTwo][pointer] = (grid[pointerTwo][pointer] + 1) % 256
- 
+    
         elif code[pc] == "[":
             if grid[pointerTwo][pointer] == 0:
                 pairs = 1
@@ -67,7 +65,7 @@ def interpret(code, debug=False):
                         pairs += 1
                     elif code[pc] == "]":
                         pairs -= 1
- 
+    
         elif code[pc] == "]":
             if grid[pointerTwo][pointer] != 0:
                 pairs = 1
@@ -77,7 +75,7 @@ def interpret(code, debug=False):
                         pairs += 1
                     elif code[pc] == "[":
                         pairs -= 1
- 
+    
         elif code[pc] == "`":
             var = map(ord, input())
             for char in var:
@@ -89,12 +87,15 @@ def interpret(code, debug=False):
             grid[pointerTwo][pointer] = ord(input()[0])
         elif code[pc] == "#":
             print(color(grid[pointerTwo][pointer], grid[pointerTwo][pointer + 1], grid[pointerTwo][pointer + 2]), end='')
+
         pc = pc + 1
- 
+    
         if debug:
             print(grid, " ", code[pc-1])
- 
- 
+    
+    
 WIDTH, HEIGHT = 20, 20
-code = ',>+255>+0>+0<<#<+32.>4+10.'
-interpret(preprocess(code))
+code = ',>+255>+255>+0<<#<+32.>4+10.'
+
+processed = preprocess(code)
+interpret(processed)
