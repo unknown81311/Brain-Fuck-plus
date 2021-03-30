635b74e5124f2945ec007dea5f35409f115b2894
Daksh <daksh.gupta@live.com>
2021-03-29 17:14:50 -0700

    Added converter files

--- converters/BfToBf+.js
+++ converters/BfToBf+.js
@@ -1,1 +1,25 @@
-// put the first conde you made here
+function convert(string, list) {
+    let start = 0;
+    let len = string.length;
+    let result = "";
+    string = string.split("");
+    for(let i = 0; i < len - 1; ++i) {
+        if(string[i] != string[i + 1]) {
+            if(i - start + 1 <= 2 || !list.includes(string[i])) {
+                result += string.join("").substring(start, i + 1);
+                start = i + 1;
+                continue;
+            }
+            result += string[i];
+            result += i - start + 1;
+            start = i + 1;
+        }
+    }
+    if(len - start <= 2 || !list.includes(string[len - 1])) {
+        result += string.join("").substring(start, len);
+        return result;
+    }
+    result += string[len - 1];
+    result += len - start;
+    return result;
+}
--- converters/StringToBf+.js
+++ converters/StringToBf+.js
@@ -1,1 +1,15 @@
-// put the last code you made here
+function convert(string) {
+    string = string.split('');
+    const len = string.length;
+    for(let i = 0; i < len; ++i) {
+        string[i] = string[i].charCodeAt();
+        if(string[i] > 127)
+            string[i] -= 256;
+    }
+    let res = (string[0] > 0 ? '+' : '') + (string[0] ? string[0] : '') + '.';
+    for(let i = 1; i < len; ++i) {
+        const diff = (string[i] + (string[i] < 0) * 256) - (string[i - 1] + (string[i - 1] < 0) * 256);
+        res += (diff > 0 ? '+' : '') + (diff ? diff : '') + '.';
+    }
+    return res;
+}
